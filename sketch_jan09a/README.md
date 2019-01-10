a75567d58e1dc26343d4c06ec83ba99c1f50abe6
glassman <realdcbrten@gmail.com>
2019-01-09 21:10:22 -0800

    Added a more refined README for sketch a discussing the code in more breadth

---
+++ sketch_jan09a/README.md
@@ -0,0 +1,84 @@
+# HyperRail Autonomous Code Concept (Group C5)
+**Group Members**:<br/>Yeshun Ma (glassman7), Bryden Kinsella, Wojtek Sobczyk, Jordan Garrison, Akhil Prabhu, Marc Vanderbanck, Audrey Alianto
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://github.com/glassman7/mech223hyperrail/blob/master/sketch_jan09a/sketch_jan09a.ino)
+
+## Directory
+1. [Key Features](#keyfeat)
+2. [Outstanding TODO's](#todo)
+3. [Brief Code Rundown](#briefrun)
+4. [Modifiable Constants](#modif)
+5. [Final Remarks](#fin)
+
+The following code pertains to the autonomous functionality of the hyperrail train design.
+
+  - Senses the opening of the starting gate
+  - Only starts moving when gate is opened
+  - Will stop moving when the round has been completed
+
+***
+### Key Features <a name="keyfeat"></a>
+
+  - Utilizes a ultrasonic sensor to detect the starting gate
+  - Takes in account of systematic error from train placement on track relative to gate and sensitivity of sensor to noise
+
+
+### Outstanding TODO's <a name="todo"></a>
+  - A physical test with Arduino has not been conducted yet
+  - Drag and drop markdown and HTML files into Dillinger
+  - Export documents as Markdown, HTML and PDF
+
+Extra batteries are permitted to power the Arduino, as they do not contribute to the direct acceleration of the device, as quoted from the HyperRail Competition PDF:
+
+> Energy from additional batteries
+is permitted provided the energy is not used to drive (accelerate) the vehicle (i.e. power
+for control electronics, actuators that do not provide propulsion and other circuits will not
+count towards energy used).
+
+*Much of the code presented in this repository is subject to change with more prototyping.  Refer to the commit history for revision changes*
+
+### Brief Code Rundown <a name="briefrun"></a>
+
+The purpose of this software repo is to automate the starting behavior of the train.
+
+* Do not start the train until the gate is opened.
+* Constantly check if the gate has been opened, only then will the code proceed to the superloop for which more complex automated code will be executed for the actual round.
+* This check is done by utilizing the ultrasonic sensor.
+* Once the starting gate has been opened, we execute the code for the round.
+* An additional check in the superloop will be made to check if the train has already completed the round.  *It is possible that this function may be omitted for simplicity*
+
+
+### Modifiable Constants <a name="modif"></a>
+
+Due to the lack of prototyping conducted, many of the constant parameters in this code will be subject to change.
+
+Gate Distance and Estimated Uncertainty
+```c
+const int GATEDIST = 400;
+const int GATEDISTERROR = 40;
+```
+
+Trigger Duration and Gap Times of the Ultrasonic Sensor
+```c
+const int TRIGDURATION = 10;
+const int RESTDURATION = 2;
+```
+
+Pin Initialization (possibly subject to change due to wiring accomodations).
+```c
+const int testPin = 8;
+const int trigPin = 9;
+const int echoPin = 10;
+```
+
+Delay Time for the setup of the train relative to the starting gate.  Subject to change from physical tests and rehearsal.
+```c
+const int DELAYTIME = 5000;
+```
+
+Ultrasonic Sensor sensitivity, again subject to possible change through experimentation.
+```c
+const int senstivity = 300;
+```
+### Final Remarks <a name="fin"></a>
+Feel free to update this README should substantial changes to the functionality of this code be made, which will most likely happen with the course of the HyperRail project.
--- sketch_jan09a/sketch_jan09a.ino
+++ sketch_jan09a/sketch_jan09a.ino
@@ -1,43 +1,56 @@
+// Initialization of constant parameters pertaining to train and start gate
+// distance, subject to change.
 const int GATEDIST = 400;
 const int GATEDISTERROR = 40;
 
+// Initialization of constant parameters pertaining to duration of the triggers
+// of the ultrasonic sensor.
+// As of now, triggers are 10 microseconds long and 2 microseconds apart.
 const int TRIGDURATION = 10;
 const int RESTDURATION = 2;
 
+
+// Arduino Pins initialization
 const int testPin = 8;
 const int trigPin = 9;
 const int echoPin = 10;
 
 
+// Time for members to place and position the train from the starting gate
+// Subject to change.
 const int DELAYTIME = 5000;
 
-int   sensorOne = 1;
-int   senstivity = 300;
-int   sensorOneSignal;
 
+// SensorOne (ultrasonic sensor) sensitivity and signal value variables
+const int senstivity = 300;
+int sensorOneSignal;
+
+// Duration and distance for sensorOne
+// Duration is in microseconds, hence a large value is expected.  Long keyword assigned.
 long  duration;
 int   distance;
 
-bool  inMotion = false;
+// Boolean Variables
+bool  completed = false;  // Has the train already completed the round?
+bool  inMotion = false;   // Has the gate already opened and the train is moving?
 
 void setup() {
-  // put your setup code here, to run once:
-
   // sleeps the program to give us time to place train on track
   // if sensor is outside the range, and it isnt already in motion begin moving to position train on the track
   delay(DELAYTIME);
-  pinMode(sensorOne, INPUT);
 
-  // Ultrasonic initialization
+  // Ultrasonic pin declaration
   pinMode(trigPin, INPUT);
   pinMode(echoPin, OUTPUT);
 
+  // Data stream between arduino and computer, limited due to demand from ultrasonic sensor
   Serial.begin(9600);
 
   // PRE-MOTION PHASE CHECK
   // reads sensor input at least once from program runtime
   // if train is in the range, set inMotion to true (train is now going to move)
   // and break from the checking phase
+  // THE PROGRAM WILL NOT CONTINUE TO THE SUPERLOOP UNTIL THE GATE HAS BEEN OPENED
   do {
     sensorOneSignal = computeDistance();
     if (!sensitivityCheck(sensorOneSignal, senstivity)) {
@@ -47,10 +60,9 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  // IN MOTION PHASE
-  moveTrain();
+  // If the round has not been completed yet, execute round code
+  if (!completed)
+    moveTrain();
 }
 
 // FUNCTION:  Returns the computed distance of the obstruction to the ultrasonic
@@ -64,9 +76,9 @@ int computeDistance() {
   // 2 microsecond delay between triggers
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(TRIGDURATION);
-  // ultrasonic sensor will fire the trigger for 10 microseconds
+  // Ultrasonic sensor will fire the trigger for 10 microseconds
 
-  // trigger pin will be terminated so the signal can be properly received
+  // Trigger pin will be terminated so the signal can be properly received
   // in the echo pin
   digitalWrite(trigPin, LOW);
 
@@ -75,16 +87,16 @@ int computeDistance() {
 
 
 
-// ASSUME:    signal has sensitivity, and distance between gate and train has
+// ASSUME:    Signal has sensitivity, and distance between gate and train has
 //            ERROR, signal is within range if the two range bands overlap
-// FUNCTION:  returns true if signal is within the GATEDIST range taking in account of
+// FUNCTION:  Returns true if signal is within the GATEDIST range taking in account of
 //            sensitivity, false otherwise.
 // PARAMETERS:
 //            signal: analog input from sensorOne
 //            sens: sensitivity of the signal
 //
-// CASE 1:    signal lowerbound overlaps GATEDIST band
-// CASE 2:    signal upperbound overlaps GATEDIST band
+// CASE 1:    Signal lowerbound overlaps GATEDIST band
+// CASE 2:    Signal upperbound overlaps GATEDIST band
 bool sensitivityCheck(int signal, int sens) {
   return (
     ((signal - sens) <= (GATEDIST + GATEDISTERROR)) ||   // CASE 1
